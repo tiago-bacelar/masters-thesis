84fd82107317f5aeea9cea403ef43404951fafea
tiago-bacelar <pereira.migueltiago@gmail.com>
2025-05-29 03:15:25 +0100

    try to add CompOrd logic to interpreter

--- Lang/Hybrid.hs
+++ Lang/Hybrid.hs
@@ -1,53 +1,73 @@
 module Lang.Hybrid where
 
+import CompOrd
+
 import Control.Monad (ap, liftM2)
 import Data.Maybe
 
 --plugging in a t less than 0 or greater than the duration should be invalid
---endpoint is memo'ed to avoid time comparisons
-newtype Hybrid t a = Hybrid (t -> a, Maybe t, a) deriving (Functor)
+--endpoint is memo'ed to avoid unnecessary time comparisons
+newtype Hybrid t a = Hybrid (t -> a, Maybe (t, a)) deriving (Functor)
 
 type HProgram t a = a -> Hybrid t a
 
 
 wait :: t -> HProgram t a
-wait t x = Hybrid (const x, Just t, x)
+wait t x = Hybrid (const x, Just (t, x))
 
 end :: HProgram t a
-end x = Hybrid (const x, Nothing, error "endpoint of infinite hybrid program")
+end x = Hybrid (const x, Nothing)
 
 for :: (t -> a) -> t -> Hybrid t a
-for f d = Hybrid (f, Just d, f d)
+for f d = Hybrid (f, Just (d, f d))
 
 forever :: (t -> a) -> Hybrid t a
-forever f = Hybrid (f, Nothing, error "endpoint of infinite hybrid program")
+forever f = Hybrid (f, Nothing)
 
 
 eval :: Hybrid t a -> t -> a
-eval (Hybrid (f, _, _)) = f --TODO: validate time?
+eval (Hybrid (f, _)) = f --TODO: validate time?
 
 duration :: Hybrid t a -> Maybe t
-duration (Hybrid (_, d, _)) = d
+duration (Hybrid (_, m)) = fmap fst m
 
 startpoint :: (Num t) => Hybrid t a -> a
-startpoint (Hybrid (f, _, _)) = f 0
+startpoint (Hybrid (f, _)) = f 0
 
 endpoint :: Hybrid t a -> a
-endpoint (Hybrid (_, _, e)) = e
+endpoint (Hybrid (_, m)) = maybe (error "endpoint of infinite hybrid program") snd m
 
---TODO: use CompOrd
-join :: (Ord t, Num t) => Hybrid t a -> Hybrid t a -> Hybrid t a
-join x y = Hybrid (maybe (eval x) h (duration x), liftM2 (+) (duration x) (duration y), endpoint y)
-    where h d t | t < d       = eval x t
+mEndpoint :: Hybrid t a -> Maybe a
+mEndpoint (Hybrid (_, m)) = fmap snd m
+
+
+join :: (Num t, Ord t) => Hybrid t a -> Hybrid t a -> Hybrid t a
+join x y = Hybrid (maybe (eval x) h (duration x), liftM2 joinDur (dur x) (dur y))
+    where dur (Hybrid (_, m)) = m
+          joinDur (t1, _) (t2, e) = (t1 + t2, e)
+          h d t | t < d       = eval x t
                 | otherwise   = eval y (t - d)
+
+joinComp :: (Num t, CompOrd t) => Int -> Hybrid t a -> Hybrid t a -> Hybrid t (Maybe a)
+joinComp n x y = Hybrid (maybe (Just . eval x) h (duration x), liftM2 joinDur (dur x) (dur y))
+    where dur (Hybrid (_, m)) = m
+          joinDur (t1, _) (t2, e) = (t1 + t2, Just e)
+          h d t = case mCompare t d n of
+                    Nothing -> Nothing
+                    Just LT -> Just $ eval x t
+                    _       -> Just $ eval y (t - d)
+
 
 compose :: (Ord t, Num t) => HProgram t a -> HProgram t a -> HProgram t a
 compose f g x = let h = f x in join h (g $ endpoint h)
 
+composeComp :: (Num t, CompOrd t) => Int -> HProgram t a -> HProgram t a -> a -> Hybrid t (Maybe a)
+composeComp n f g x = let h = f x in joinComp n h (g $ endpoint h)
 
-instance (Ord t, Num t) => Applicative (Hybrid t) where
+
+instance (Num t, Ord t) => Applicative (Hybrid t) where
     pure  = wait 0
     (<*>) = ap
 
-instance (Ord t, Num t) => Monad (Hybrid t) where
+instance (Num t, Ord t) => Monad (Hybrid t) where
     h >>= f = join (fmap (startpoint . f) h) (endpoint $ fmap f h) --can this be optimized?
--- Lang/Interpreter.hs
+++ Lang/Interpreter.hs
@@ -1,14 +1,18 @@
 module Lang.Interpreter where
 
 import CompReal
+import CompOrd
 import Solver.Powers
 import Solver.Interval
 import Solver.FAD
 import Solver.Solver
-import Lang.Hybrid hiding (wait, end)
-import Lang.Parser
+import Lang.Hybrid
+import Lang.Parser hiding (Comparator(..))
+import qualified Lang.Parser as Lang
 
-import Prelude hiding (Ordering(..))
+import Control.Monad (ap)
+import Control.Monad.State.Lazy (evalState, get, put)
+import qualified Control.Monad.State.Lazy as MS (State)
 import Data.Ratio
 import Data.Map as Map (Map, (!), empty, insert)
 import qualified Data.Map as Map (map)
@@ -18,19 +22,49 @@ import qualified Data.Map as Map (map)
 --the comparisons problem should be studied closer later on (probably need two modes: strict comparison, which guarantees
 --correctness but throws PLE, and a parametrized lax mode that assumes equality after specified precision is exhausted)
 
---TODO: change state to list/vector
-type State r = (r, Map Ident r)
-type RunnableProgram r = HProgram r (State r) --State r -> Hybrid r (State r)
+data RunResult a = Val a | Err String deriving (Functor)
+type EState = (Int, Int) --comparison precision, loop iterations
+newtype E r a = E { runE :: MS.State EState (Hybrid r (RunResult a)) }
 
-initial :: (Num r) => State r
+instance (Num r) => Applicative (E r) where
+    pure = E . return . pure . Val
+    (<*>) = ap
+
+instance (Num r) => Monad (E r) where
+    x >>= f = E $ do { es <- get; return $ evalE x es >>= (\y -> evalE (lifted y) es) }
+        where lifted (Val x) = f x
+              lifted (Err e) = failE e
+
+evalE :: E r a -> EState -> (Hybrid r (RunResult a))
+evalE e = evalState (runE e)
+
+failE :: String -> E r a
+failE = E . return . end . Err
+
+getCmp :: E r Int
+getCmp = E $ fmap (pure . Val . fst) get
+
+decIter :: E r ()
+decIter = E $ do
+    (cmp, n) <- get
+    if n <= 0
+    then return $ end $ Err "Iteration limit exhausted"
+    else do { put (cmp, n-1); return $ pure $ Val () }
+
+
+--TODO: change state to list/vector
+type PState r = (r, Map Ident r)
+type RunnableProgram r = PState r -> E r (PState r)
+
+initial :: (Num r) => PState r
 initial = (0, empty)
 
---Redefined wait and end since they aren't "pure" (the time var still has to be updated)
-wait :: (Num r) => r -> RunnableProgram r
-wait d (ti, vars) = for (\t -> (ti + t, vars)) d
+--Redefined wait and end to update time and return the right type TODO
+waitE :: (Num r) => r -> HProgram r (PState r)
+waitE d (ti, vars) = for (\t -> (ti + t, vars)) d
 
-end :: (Num r) => RunnableProgram r
-end (ti, vars) = forever (\t -> (ti + t, vars))
+endE :: (Num r) => HProgram r (PState r)
+endE (ti, vars) = forever (\t -> (ti + t, vars))
 
 
 evalFunc :: (Floating r) => Function -> r -> r
@@ -49,28 +83,27 @@ evalOp Div  = (/)
 evalOp Pow  = (**) --TODO: pow with Powers class
 evalOp Log  = logBase
 
-evalExpr :: (Floating r, Powers r) => Expr -> State r -> r
+evalExpr :: (Floating r, Powers r) => Expr -> PState r -> r
 evalExpr (Var T) s     = fst s
 evalExpr (Var (V v)) s = snd s ! v
 evalExpr (Num x) s     = anyFloat x
 evalExpr (Func f a) s  = evalFunc f (evalExpr a s)
 evalExpr (Op op a b) s = evalOp op (evalExpr a s) (evalExpr b s)
 
---TODO: comparison precision
-evalComp :: (Ord r) => Comparator -> r -> r -> Bool
-evalComp LT  = (<)
-evalComp GT  = (>)
-evalComp LEQ = (<=)
-evalComp GEQ = (>=)
-evalComp EQ  = (==)
-evalComp NEQ = (/=)
+evalComp :: (CompOrd r) => Lang.Comparator -> r -> r -> Int -> Maybe Bool
+evalComp Lang.LT  x y = fmap (LT ==) . mCompare x y
+evalComp Lang.GT  x y = fmap (GT ==) . mCompare x y
+evalComp Lang.LEQ x y = fmap (GT /=) . mCompare x y
+evalComp Lang.GEQ x y = fmap (LT /=) . mCompare x y
+evalComp Lang.EQ  x y = fmap (EQ ==) . mCompare x y
+evalComp Lang.NEQ x y = fmap (EQ /=) . mCompare x y
 
-evalB :: (Floating r, Powers r, Ord r) => BTerm -> State r -> Bool
-evalB (BConst b) s   = b
-evalB (Comp c a b) s = evalComp c (evalExpr a s) (evalExpr b s)
+evalB :: (Floating r, Powers r, CompOrd r) => BTerm -> Int -> PState r -> Maybe Bool
+evalB (BConst b) n s   = Just b
+evalB (Comp c a b) n s = evalComp c (evalExpr a s) (evalExpr b s) n
 
 --TODO: optimization? identify common expressions and turn them into variables (or do it in the parsing step?)
-evalFor :: (Floating r, Powers r) => [(Ident, Expr)] -> State r -> r -> State r
+evalFor :: (Floating r, Powers r) => [(Ident, Expr)] -> PState r -> r -> PState r
 evalFor rs (t0, vars) = aux
     where x0 = map ((vars!) . fst) rs
           consts = Map.map con vars
@@ -79,20 +112,43 @@ evalFor rs (t0, vars) = aux
           aux dt = (t, foldr (uncurry insert) vars $ zip (map fst rs) $ solve f t0 x0 t)
             where t = t0+dt
 
-interpret :: (Floating r, Powers r, Ord r) => Program -> RunnableProgram r
-interpret (Assign v e) s       = pure $ (fst s, insert v (evalExpr e s) $ snd s)
-interpret (For [] (Just t)) s  = wait (evalExpr t s) s              --TODO: validate t (>= 0)
-interpret (For [] Nothing) s   = end s
-interpret (For rs (Just t)) s  = for (evalFor rs s) (evalExpr t s)  --TODO: validate t (>= 0)
-interpret (For rs Nothing) s   = forever (evalFor rs s)
-interpret (IfThenElse c p q) s = if evalB c s then interpret p s else interpret q s
-interpret (WhileDo c p) s      = if evalB c s then compose (interpret p) (interpret (WhileDo c p)) s else pure s
-interpret (Seq p q) s          = compose (interpret p) (interpret q) s
-interpret Nop s                = pure s
 
+fromHybrid :: (Hybrid r a) -> E r a
+fromHybrid = E . return . fmap Val
 
-run :: (Num r) => RunnableProgram r -> r -> Map Ident r
-run p = snd . eval (p initial)
+fromMHybrid :: (Hybrid r (Maybe a)) -> E r a
+fromMHybrid = E . return . fmap aux
+    where aux (Just x) = Val x
+          aux Nothing  = Err "Precision exhausted in composition" --TODO: separate error for this
 
-query :: (Num r) => RunnableProgram r -> r -> Ident -> r
-query p = (!) . run p
+validateT :: r -> E r ()
+validateT d = do
+    cmp <- getCmp
+    if (d >! 0) cmp
+    then return ()
+    else failE ("Time step is not verifiably positive (comparison precision exhausted): " ++ show d)
+
+compareE :: BTerm -> PState r -> E r Bool
+compareE c s = do
+    cmp <- getCmp
+    case evalB c cmp s of
+        Just b  -> return b
+        Nothing -> failE "Precision exhausted in comparison"
+
+smth :: RunnableProgram r -> EState -> State r -> Hybrid r (RunResult (PState r))
+smth p es s = evalE (p s) es
+
+interpret :: (Floating r, Powers r, CompOrd r) => Program -> RunnableProgram r
+interpret Nop s                = return s
+interpret (Assign v e) s       = return (fst s, insert v (evalExpr e s) $ snd s)
+interpret (For [] (Just t)) s  = let d = evalExpr t s in do { validateT d; fromHybrid $ waitE d s }
+interpret (For [] Nothing) s   = fromHybrid $ endE s
+interpret (For rs (Just t)) s  = let d = evalExpr t s in do { validateT d; fromHybrid $ for (evalFor rs s) d }
+interpret (For rs Nothing) s   = fromHybrid $ forever $ (evalFor rs s)
+interpret (IfThenElse c p q) s = do { b <- compareE c s; if b then interpret p s else interpret q s }
+interpret (WhileDo c p) s      = do { b <- compareE c s; if b then interpret (Seq p (WhileDo c p)) s else return s }
+interpret (Seq p q) s          = do { x; cmp <- getCmp; y; fromMHybrid $ composeComp cmp (evalE . interpret p) (evalE . interpret q) s }
+    where x = interpret p s --TODO
+
+run :: (Num r) => RunnableProgram r -> EState -> r -> RunResult (Map Ident r)
+run p es = fmap snd . eval (evalE (p initial) es)
